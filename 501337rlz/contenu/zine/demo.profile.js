17289512f0ee6a8b2c0bd63bc9affb7395bc7a90
Xyl2k <xylitol@temari.fr>
2016-02-14 19:10:32 +0100

    Initial commit
    Initial commit

---
+++ 501337rlz/contenu/zine/demo.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../contenu/zine/src.js",
+			resourceName: "demos.castle.src",
+			dependencies: [
+				"demos.castle.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
